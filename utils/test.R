1abd7ddd99b171e1edce91a706b26ac43ce22146
yangchen <yangchenxd@foxmail.com>
2019-10-15 19:07:30 +0800

    first commit

---
+++ utils/test.R
@@ -0,0 +1,10 @@
+source("myread.ENVI.R")
+source("imagematrix.R")
+require(ggplot2)
+require(reshape2)
+require(ggthemes)
+
+
+imagepath <- "../Statistics-SAR-Intensity-master 2/Data/Images/ESAR/"
+
+print('hello')
